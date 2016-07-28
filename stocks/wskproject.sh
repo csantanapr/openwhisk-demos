b7eef941dae4ed0132ddf4ea39b1d3eb097d81e6
Carlos Santana <csantana@us.ibm.com>
2016-07-28 10:25:40 -0400

    fix problem with curl now runs on any openwhisk system

--- stocks/wskproject.sh
+++ stocks/wskproject.sh
@@ -9,12 +9,23 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-# capture the namespace where actions will be created
-CURRENT_NAMESPACE=`wsk property get --namespace | awk '{print $3}'`
-echo "Current namespace is $CURRENT_NAMESPACE."
+# capture current state of wsk
+CURRENT_NAMESPACE=`wsk property get --namespace | awk '{printf $3}'`
+echo "Current namespace is ${CURRENT_NAMESPACE}."
+
+CURRENT_APIHOST=`wsk property get --apihost | awk '{printf $4}'`
+echo "Current apihost is ${CURRENT_APIHOST}."
+
+CURRENT_AUTH=`wsk property get --auth | awk '{printf $3}'`
+#echo "Current auth is ${CURRENT_AUTH}."
+
+CURRENT_AUTH_HEADER="Authorization: Basic $(echo -n ${CURRENT_AUTH} | base64)"
+#echo "Current auth header is ${CURRENT_AUTH_HEADER}"
+
+WSK_CONTENT_HEADER='Content-Type: application/json'
 
 function usage() {
-  echo -e "${YELLOW}Usage: $0 [--install, --test, --update, --reinstall]${NC}"
+  echo -e "${YELLOW}Usage: $0 [--install, --test, --update, --reinstall, --uninstall]${NC}"
 }
 
 function install() {
@@ -66,15 +77,37 @@ function test() {
   echo "Invoke Action directly via wsk cli"
   wsk action invoke processStocks -b -r -p IBM 200
   
+
+
   echo "Invoke Action via curl using HTTP POST"
-  curl -d '{ "IBM": "200" }' "https://openwhisk.ng.bluemix.net/api/v1/namespaces/$CURRENT_NAMESPACE/actions/processStocks?blocking=true&result=true" -XPOST -H 'Content-Type: application/json' -H "Authorization: Basic $(wsk property get --auth | awk '{print $3}' | base64)"
+  WSK_ACTION="processStocks"
+  WSK_PARAMS='{ "IBM": "200" }'
+  WSK_OPTS='?blocking=true&result=true'
+  WSK_PATH="https://${CURRENT_APIHOST}/api/v1/namespaces/${CURRENT_NAMESPACE}/actions/${WSK_ACTION}${WSK_OPTS}"
+  curl -d "${WSK_PARAMS}" -XPOST -H "${WSK_CONTENT_HEADER}" -H "${CURRENT_AUTH_HEADER}" "${WSK_PATH}"
   echo ""
 
   echo "Fire Trigger stockTrigger via wsk cli"
   wsk trigger fire stockTrigger '{"IBM":200}'
 
   echo "Fire Trigger via curl using HTTP POST"
-  curl -d '{ "IBM": "200" }' "https://openwhisk.ng.bluemix.net/api/v1/namespaces/$CURRENT_NAMESPACE/triggers/stockTrigger" -XPOST -H 'Content-Type: application/json' -H "Authorization: Basic $(wsk property get --auth | awk '{print $3}' | base64)"
+  WSK_TRIGGER="stockTrigger"
+  WSK_PARAMS='{ "IBM": "200" }'
+  WSK_PATH="https://${CURRENT_APIHOST}/api/v1/namespaces/${CURRENT_NAMESPACE}/triggers/${WSK_TRIGGER}"
+  
+  CURL_RESULT=`curl -d "${WSK_PARAMS}" -XPOST -H "${WSK_CONTENT_HEADER}" -H "${CURRENT_AUTH_HEADER}" "${WSK_PATH}"`
+  echo ${CURL_RESULT}
+  WSK_TRIGGER_ACTIVATION=`echo ${CURL_RESULT} | grep activationId | awk '{printf $3}' | tr -d '"'`
+  echo "Trigger activation id is ${WSK_TRIGGER_ACTIVATION}"
+  echo ""
+  
+  echo "Fetch the results from the activation via wsk cli"
+  wsk activation get "${WSK_TRIGGER_ACTIVATION}"
+  
+
+  echo "Fetch the results from the activation via HTTP GET"
+  WSK_PATH="https://${CURRENT_APIHOST}/api/v1/namespaces/_/activations/${WSK_TRIGGER_ACTIVATION}"
+  curl -XGET -H "${WSK_CONTENT_HEADER}" -H "${CURRENT_AUTH_HEADER}" "${WSK_PATH}"
   echo ""
 
   echo -e "${GREEN}Test Complete${NC}"
