5f963f7ab0dda136579ac0b89a0854d6615287aa
Carlos Santana <csantana@us.ibm.com>
2018-01-30 18:02:50 -0500

    update deploy sh to use .env

--- python3IBM/deploy.sh
+++ python3IBM/deploy.sh
@@ -6,7 +6,7 @@ bx wsk action update translateForm translateForm.py --kind python-jessie:3
 bx wsk action invoke translateForm -p payload "Muy buen servicio" -p customer_id 101 -r
 
 #bx wsk action update storeFormDB storeFormDB.py --kind python-jessie:3
-bx wsk action update storeFormDB storeFormDB.py -P dbcredsparams.json --kind python-jessie:3
+bx wsk action update storeFormDB storeFormDB.py --kind python-jessie:3 -P .env.json 
 #bx wsk action update storeFormDB storeFormDB.py --docker ibmfunctions/action-python-v3
 bx wsk action invoke storeFormDB -p payload "Very good service" -p customer_id 101 -r
 
@@ -15,11 +15,11 @@ bx wsk service bind language_translator translateForm
 
 
 bx wsk action update myCustomerFormSeq translateForm,storeFormDB --sequence
-bx wsk action invoke myCustomerFormSeq -p payload "Muy buen servicio" -p customer_id 102 -r
+bx wsk action invoke myCustomerFormSeq -p payload "Muy buen servicio" -p customer_id 101 -r
 
-#bx wsk action update setupDB setupDB.py -P dbcredsparams.json --kind python-jessie:3
+#bx wsk action update setupDB setupDB.py -P .env.json --kind python-jessie:3
 #bx wsk action update setupDB setupDB.py --kind python-jessie:3
 #bx wsk action invoke setupDB -b
 #bx wsk action get setupDB parameters
 
-bx wsk activation get --last
+#bx wsk activation get --last
