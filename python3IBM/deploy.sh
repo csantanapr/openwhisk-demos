ec2673ba28a246ce95aa6f310b0975afea24942a
Carlos Santana <csantana@us.ibm.com>
2018-02-02 20:16:17 -0500

    upload readme

--- python3IBM/deploy.sh
+++ python3IBM/deploy.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-#When availbel in prod
-bx wsk action update translateForm translateForm.py --kind python-jessie:3
-#bx wsk action update translateForm translateForm.py --docker ibmfunctions/action-python-v3
-bx wsk action invoke translateForm -p payload "Muy buen servicio" -p customer_id 101 -r
-
-#bx wsk action update storeFormDB storeFormDB.py --kind python-jessie:3
-bx wsk action update storeFormDB storeFormDB.py --kind python-jessie:3 -P .env.json 
-#bx wsk action update storeFormDB storeFormDB.py --docker ibmfunctions/action-python-v3
-bx wsk action invoke storeFormDB -p payload "Very good service" -p customer_id 101 -r
-
-bx wsk service bind language_translator translateForm
-#bx wsk service bind dashDB storeFormDB --instance Db2WarehouseOnCloud
-
-
-bx wsk action update myCustomerFormSeq translateForm,storeFormDB --sequence
-bx wsk action invoke myCustomerFormSeq -p payload "Muy buen servicio" -p customer_id 101 -r
-
+# Setup DB
 #bx wsk action update setupDB setupDB.py -P .env.json --kind python-jessie:3
 #bx wsk action update setupDB setupDB.py --kind python-jessie:3
 #bx wsk action invoke setupDB -b
-#bx wsk action get setupDB parameters
 
+# Deploy actions
+bx wsk action update translateForm translateForm.py --kind python-jessie:3
+bx wsk action update storeFormDB storeFormDB.py --kind python-jessie:3
+#bx wsk action update storeFormDB storeFormDB.py --kind python-jessie:3 -P .env.json 
+
+# Setup credentials
+bx wsk service bind language_translator translateForm
+bx wsk service bind dashDB storeFormDB --instance Db2WarehouseOnCloud
+
+# Unit test
+bx wsk action invoke translateForm -p payload "Muy buen servicio" -p customer_id 101 -r
+bx wsk action invoke storeFormDB -p payload "Very good service" -p customer_id 101 -r
+
+# Create Sequence
+bx wsk action update myCustomerFormSeq translateForm,storeFormDB --sequence
+bx wsk action invoke myCustomerFormSeq -p payload "Muy buen servicio" -p customer_id 101 -r
+
+ # Data Science
 bx wsk action update pandas pandas.py --kind python-jessie:3
 bx wsk action invoke pandas -b
-
 bx wsk activation get --last
